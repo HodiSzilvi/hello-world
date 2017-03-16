b0e32b825b86d6ce08ca298815143b3985bbb83e
HodiSzilvi <szhodvogner@gmail.com>
2017-03-16 20:50:40 +0100

    Create Alagut.java
    ASDasélkdalskdéasdhsafa

---
+++ Alagut.java
@@ -0,0 +1,30 @@
+public class Alagut {
+	private List<Sin> tunnel = new List<Sin>();
+	private KulonlegesHely exitA;
+	private KulonlegesHely exitB;
+	
+	Alagut(){
+		
+	}
+	
+	public Sin build(KulonlegesHely hely){
+		//felépíti a kapcsolatot a különleges hellyel
+		System.out.println("build() metódus lefutott");
+		return Sin;
+		
+	}
+	
+	public Sin destroy(KulonlegesHely hely){
+		System.out.println("destroy() metódus lefutott");
+		return Sin;
+	}
+	
+	public void setExitA(KulonlegesHely a){
+		System.out.println("setExitA() metódus lefutott");
+	}
+	
+	public void setExitB(KulonlegesHely b){
+		System.out.println("setExitB() metódus lefutott");
+	}
+
+}
